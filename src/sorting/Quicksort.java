88f1ab9a412d7bf02053109067563ad35512ea7e
Ang <woeihaw94@gmail.com>
2025-12-29 07:51:33 +0800

    add quick sort

---
+++ src/sorting/Quicksort.java
@@ -0,0 +1,48 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class Quicksort {
+
+    public static void quickSortHelper(int[] array, int left, int right){
+        if(left<right){
+            int pivotIndex = pivot(array,left,right);
+            quickSortHelper(array,left,pivotIndex-1);
+            quickSortHelper(array,pivotIndex+1,right);
+        }
+
+    }
+
+    public static void quickSort(int[] array){
+        quickSortHelper(array,0,array.length-1);
+    }
+
+    private static int pivot(int[] array, int pivotIndex, int endIndex){
+        int swapIndex = pivotIndex;
+
+        for(int i = pivotIndex+1; i<= endIndex;i++){
+            if(array[i] <array[pivotIndex]){
+                swapIndex++;
+                swap(array,swapIndex,i);
+            }
+        }
+        swap(array, pivotIndex,swapIndex);
+        return swapIndex;
+    }
+
+    private static void swap(int[] array, int firstIndex, int secondIndex){
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] myArrray = {4,6,1,7,3,2,5};
+//        int returnIndex = pivot(myArrray,0,6);
+//        System.out.println("Returned Index: " + returnIndex);
+//        System.out.println(Arrays.toString(myArrray));
+
+        quickSort(myArrray);
+        System.out.println(Arrays.toString(myArrray));
+    }
+}
