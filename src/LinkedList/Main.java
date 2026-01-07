d3a40567c879bd3eaee8da8b8e8b77e8466494a5
Ang <woeihaw94@gmail.com>
2026-01-08 07:29:48 +0800

    initialize linkedlist

---
+++ src/LinkedList/Main.java
@@ -0,0 +1,11 @@
+package LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinedList myLinkedList = new LinedList(1);
+
+      myLinkedList.append(2);
+
+        myLinkedList.printList();
+    }
+}
