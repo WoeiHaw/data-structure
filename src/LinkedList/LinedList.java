cc9358326af0c048fd8a7ea937059abe0b1153be
Ang <woeihaw94@gmail.com>
2026-01-09 07:42:16 +0800

    add linkedlist

--- src/LinkedList/LinedList.java
+++ src/LinkedList/LinedList.java
@@ -48,5 +48,39 @@ public class LinedList {
         length++;
 
     }
+    public Node removeLast(){
+        if(length == 0) return null;
+        Node temp = head ;
+        Node pre = head;
+
+        while (temp.next != null){
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+
+        length--;
+
+        if(length ==0){
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+
+    public void prepend(int value){
+        Node newNode = new Node(value);
+        if(length ==0){
+            head = newNode;
+            tail = newNode;
+        }else{
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
+
+
 
 }
