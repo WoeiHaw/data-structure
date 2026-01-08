571e1f5bff43dfac3142728b32612ee625c7a7a8
Ang <woeihaw94@gmail.com>
2026-01-09 07:42:16 +0800

    add linkedlist

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -2,10 +2,15 @@ package LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LinedList myLinkedList = new LinedList(1);
+        LinedList myLinkedList = new LinedList(2);
 
-      myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.prepend(1);
 
         myLinkedList.printList();
+
+//        System.out.println(myLinkedList.removeLast().value);
+//        System.out.println(myLinkedList.removeLast().value);
+//        System.out.println(myLinkedList.removeLast());
     }
 }
