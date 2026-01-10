686aa462228b87fc3269bd50c693e4c16c0e9a26
Ang <woeihaw94@gmail.com>
2026-01-10 16:37:03 +0800

    add linkedlist

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -2,15 +2,62 @@ package LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LinedList myLinkedList = new LinedList(2);
+//        LinedList myLinkedList = new LinedList(2);
 
-        myLinkedList.append(3);
-        myLinkedList.prepend(1);
-
-        myLinkedList.printList();
+//        myLinkedList.append(3);
+//        myLinkedList.prepend(1);
+//
+//        myLinkedList.printList();
 
 //        System.out.println(myLinkedList.removeLast().value);
 //        System.out.println(myLinkedList.removeLast().value);
 //        System.out.println(myLinkedList.removeLast());
+
+//        myLinkedList.append(1);
+//        myLinkedList.printList();
+//
+//        System.out.println(myLinkedList.removeFirst().value);
+//        System.out.println(myLinkedList.removeFirst().value);
+//        System.out.println(myLinkedList.removeFirst());
+
+//        LinedList myLinKedList = new LinedList(0);
+//        myLinKedList.append(1);
+//        myLinKedList.append(2);
+//        myLinKedList.append(3);
+//
+//        System.out.println(myLinKedList.get(2).value + "\n");
+//        myLinKedList.printList();
+
+//        LinedList myLinkedList = new LinedList(11);
+//        myLinkedList.append(3);
+//        myLinkedList.append(23);
+//        myLinkedList.append(7);
+//        myLinkedList.printList();
+//        myLinkedList.set(1,4);
+//        myLinkedList.printList();
+
+//        LinedList myLinkedList = new LinedList(0);
+//        myLinkedList.append(2);
+//        myLinkedList.insert(1,1);
+//        myLinkedList.printList();
+
+//        LinedList myLinkedList = new LinedList(11);
+//        myLinkedList.append(3);
+//        myLinkedList.append(23);
+//        myLinkedList.append(7);
+//
+//        myLinkedList.remove(2);
+//        myLinkedList.printList();
+
+        LinedList myLinkedList = new LinedList(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
+        myLinkedList.reverse();
+        myLinkedList.printList();
     }
+
+
+
+
 }
