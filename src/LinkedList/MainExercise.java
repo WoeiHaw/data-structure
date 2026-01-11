015fc17bc798d14759c07e587367c7b4c7c9cff3
Ang <woeihaw94@gmail.com>
2026-01-12 07:55:44 +0800

    add linkedlist exercise

---
+++ src/LinkedList/MainExercise.java
@@ -0,0 +1,28 @@
+package LinkedList;
+
+public class MainExercise {
+
+    public static void main(String[] args) {
+
+        LinkedListExercise myLinkedList = new LinkedListExercise(1);
+        myLinkedList.append(2);
+
+        // (2) Items - Returns 2 Node
+        System.out.println(myLinkedList.removeLast().value);
+        // (1) Item - Returns 1 Node
+        System.out.println(myLinkedList.removeLast().value);
+        // (0) Items - Returns null
+        System.out.println(myLinkedList.removeLast());
+
+
+    	/*
+        	EXPECTED OUTPUT:
+        	----------------
+        	2
+        	1
+        	null
+
+     	*/
+
+    }
+}
