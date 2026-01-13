fc36016b9242316875b0b95e5d8b3b887b056e8e
Ang <woeihaw94@gmail.com>
2026-01-14 07:42:22 +0800

    add linkedlist exercise

--- src/LinkedList/LinkedListExercise.java
+++ src/LinkedList/LinkedListExercise.java
@@ -171,5 +171,19 @@ public class LinkedListExercise {
         length--;
         return temp;
     }
+
+    public void reverse(){
+        Node temp = head;
+        head = tail;
+        tail = temp;
+        Node after = temp.next;
+        Node before = null;
+        for(int i = 0 ; i<length;i++){
+            after = temp.next;
+            temp.next = before;
+            before = temp;
+            temp = after;
+        }
+    }
 }
 
