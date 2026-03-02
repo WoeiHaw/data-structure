2e3bcda358a02128d674f3fbdc642fbffb83b5b1
Ang <woeihaw94@gmail.com>
2026-03-03 07:20:38 +0800

    initialize DoublyLinkedList

---
+++ src/doublylinkedlist/Main.java
@@ -0,0 +1,17 @@
+package doublylinkedlist;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DoublyLinkedList myDLL = new DoublyLinkedList(1);
+//        myDLL.getHead();
+//        myDLL.getTail();
+//        myDLL.getLength();
+        myDLL.append(2);
+
+        System.out.println(myDLL.removeLast().value);
+        System.out.println(myDLL.removeLast().value);
+        System.out.println(myDLL.removeLast());
+        myDLL.printList();
+    }
+}
