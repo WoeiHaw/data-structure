699f533bb48ecd314901632784c9ee3e468265ff
Ang <woeihaw94@gmail.com>
2026-03-04 07:30:31 +0800

    add DoublyLinkedList

--- src/doublylinkedlist/DoublyLinkedList.java
+++ src/doublylinkedlist/DoublyLinkedList.java
@@ -69,4 +69,32 @@ public class DoublyLinkedList {
         length--;
         return temp;
     }
+
+    public void prepend(int value){
+        Node newNode = new Node(value);
+        if(length ==0){
+            head = newNode;
+            tail = newNode;
+        }else{
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        length++;
+    }
+
+    public Node removeFirst(){
+        if(length == 0) return null;
+        Node temp = head;
+        if(length ==1){
+            head = null;
+            tail = null;
+        }else{
+          head = head.next;
+          head.prev = null;
+          temp.next = null;
+        }
+        length--;
+        return temp;
+    }
 }
--- src/doublylinkedlist/Main.java
+++ src/doublylinkedlist/Main.java
@@ -3,15 +3,28 @@ package doublylinkedlist;
 public class Main {
 
     public static void main(String[] args) {
-        DoublyLinkedList myDLL = new DoublyLinkedList(1);
+//        DoublyLinkedList myDLL = new DoublyLinkedList(1);
 //        myDLL.getHead();
 //        myDLL.getTail();
 //        myDLL.getLength();
-        myDLL.append(2);
+//        myDLL.append(2);
 
-        System.out.println(myDLL.removeLast().value);
-        System.out.println(myDLL.removeLast().value);
-        System.out.println(myDLL.removeLast());
-        myDLL.printList();
+//        System.out.println(myDLL.removeLast().value);
+//        System.out.println(myDLL.removeLast().value);
+//        System.out.println(myDLL.removeLast());
+//        myDLL.printList();
+
+        DoublyLinkedList myDLL = new DoublyLinkedList(2);
+        myDLL.append(3);
+        myDLL.prepend(1);
+
+        System.out.println(myDLL.removeFirst().value);
+
+        System.out.println(myDLL.removeFirst().value);
+
+        System.out.println(myDLL.removeFirst().value);
+        System.out.println(myDLL.removeFirst());
+
+
     }
 }
