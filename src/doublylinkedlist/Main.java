907ec1460bc8914ae09b1987e4c3720064fd5d27
Ang <woeihaw94@gmail.com>
2026-03-06 07:26:30 +0800

    add DoublyLinkedList

--- src/doublylinkedlist/Main.java
+++ src/doublylinkedlist/Main.java
@@ -14,16 +14,46 @@ public class Main {
 //        System.out.println(myDLL.removeLast());
 //        myDLL.printList();
 
-        DoublyLinkedList myDLL = new DoublyLinkedList(2);
-        myDLL.append(3);
-        myDLL.prepend(1);
+//        DoublyLinkedList myDLL = new DoublyLinkedList(2);
+//        myDLL.append(3);
+//        myDLL.prepend(1);
+//
+//        System.out.println(myDLL.removeFirst().value);
+//
+//        System.out.println(myDLL.removeFirst().value);
+//
+//        System.out.println(myDLL.removeFirst().value);
+//        System.out.println(myDLL.removeFirst());
 
-        System.out.println(myDLL.removeFirst().value);
+//        DoublyLinkedList myDLL = new DoublyLinkedList(0);
+//        myDLL.append(1);
+//        myDLL.append(2);
+//        myDLL.append(3);
+//
+//        System.out.println(myDLL.get(1).value);
+//        System.out.println(myDLL.get(2).value+"\n");
+//        myDLL.printList();
 
-        System.out.println(myDLL.removeFirst().value);
+//        DoublyLinkedList myDLL = new DoublyLinkedList(11);
+//        myDLL.append(3);
+//        myDLL.append(23);
+//        myDLL.append(7);
+//
+//        myDLL.set(1,4);
+//        myDLL.printList();
 
-        System.out.println(myDLL.removeFirst().value);
-        System.out.println(myDLL.removeFirst());
+//        DoublyLinkedList myDLL = new DoublyLinkedList(1);
+//        myDLL.append(3);
+//
+//        myDLL.insert(1,2);
+//        myDLL.printList();
+
+        DoublyLinkedList myDLL = new DoublyLinkedList(0);
+        myDLL.append(1);
+        myDLL.append(2);
+
+        myDLL.remove(1);
+        myDLL.printList();
 
 
     }
