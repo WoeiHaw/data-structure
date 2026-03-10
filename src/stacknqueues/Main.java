e4ed704e78acc3b29d72c34862e5fba88b69c5bb
woeihaw.ang <woeihaw.ang@hytechc.com>
2026-03-11 07:43:02 +0800

    Add stack

---
+++ src/stacknqueues/Main.java
@@ -0,0 +1,25 @@
+package stacknqueues;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Stack myStack = new Stack(4);
+//        myStack.getTop();
+//        myStack.getHeight();
+//
+//        myStack.printStack();
+
+//        Stack myStack = new Stack(2);
+//        myStack.push(1);
+//        myStack.printStack();
+
+        Stack myStack = new Stack(7);
+        myStack.push(23);
+        myStack.push(3);
+        myStack.push(11);
+        myStack.pop();
+        myStack.printStack();
+
+
+    }
+}
