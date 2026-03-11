3871c924b8fb073045955d28a1c015feee10318c
woeihaw.ang <woeihaw.ang@hytechc.com>
2026-03-12 07:43:22 +0800

    Add queue

---
+++ src/queue/Queue.java
@@ -0,0 +1,42 @@
+package queue;
+
+public class Queue {
+    private Node first;
+    private Node last;
+    private int length;
+
+    public Queue(int value){
+        Node newNode = new Node(value);
+        first = newNode;
+        last = newNode;
+        length =1;
+    }
+    class Node {
+        int value;
+        Node next;
+
+        public  Node(int value){
+            this.value = value;
+        }
+    }
+
+    public void printQueue(){
+        Node temp = first;
+        while (temp != null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getFirst(){
+        System.out.println("First: " + first.value);
+    }
+
+    public void getLast(){
+        System.out.println("Last: " + last.value);
+    }
+
+    public void getLength(){
+        System.out.println("Length: " + length);
+    }
+}
--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -1,4 +1,4 @@
-package stacknqueues;
+package stack;
 
 public class Stack {
     private Node top;
