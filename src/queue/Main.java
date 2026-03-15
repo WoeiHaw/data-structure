355b96ee98d11dfbf40b3078daf5c308cf1e3bb5
woeihaw.ang <woeihaw.ang@hytechc.com>
2026-03-16 07:15:04 +0800

    Add queue

--- src/queue/Main.java
+++ src/queue/Main.java
@@ -4,11 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Queue myQueue = new Queue(7);
-        myQueue.getFirst();
-        myQueue.getLast();
-        myQueue.getLength();
+//        Queue myQueue = new Queue(7);
+//        myQueue.getFirst();
+//        myQueue.getLast();
+//        myQueue.getLength();
+//
+//        myQueue.printQueue();
 
+//        Queue myQueue = new Queue(1);
+//        myQueue.enqueue(2);
+//        myQueue.printQueue();
+
+        Queue myQueue = new Queue(2);
+        myQueue.enqueue(1);
+
+        System.out.println(myQueue.dequeue().value);
+        System.out.println(myQueue.dequeue().value);
+        System.out.println(myQueue.dequeue());
         myQueue.printQueue();
 
     }
--- src/queue/Queue.java
+++ src/queue/Queue.java
@@ -39,4 +39,30 @@ public class Queue {
     public void getLength(){
         System.out.println("Length: " + length);
     }
+
+    public void enqueue(int value){
+        Node newNode = new Node(value);
+        if(length ==0){
+            first = newNode;
+            last = newNode;
+        }else{
+            last.next = newNode;
+            last = newNode;
+        }
+        length++;
+    }
+
+    public Node dequeue(){
+        if(length ==0) return null;
+        Node temp = first;
+        if(length ==1){
+            first = null;
+            last = null;
+        }else{
+            first = first.next;
+            temp.next = null;
+        }
+        length--;
+        return temp;
+    }
 }
